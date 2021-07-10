c9045c3e49ebafbff2a14c4eadaf34baa17e5370
Abdallah-Shaban88 <80660937+Abdallah-Shaban88@users.noreply.github.com>
2021-07-10 20:53:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+
+import {planets} from './planets.js';
+
+ console.log(planets) 
 const slider = document.querySelector('.slider'),
-            sliderCards =Array.from( document.querySelectorAll('.planet-card'));
+      sliderCards =Array.from( document.querySelectorAll('.planet-card')),
+      planetName = Array.from(document.querySelectorAll('.planet-name')),
+      planetDetails = Array.from(document.querySelectorAll('.planet-details')),
+      planetsDescription = Array.from(document.querySelectorAll('.planet-info article'))
       let currentSlide= 0;
-      
+     
      for(let i = 1; i <= sliderCards.length; i++){ 
        document.querySelector('.bullets').innerHTML +=`
         <div class="bullet" order="${i}"> 
         <img src="./images/icon${i}.png"/>
         </div>`;
      }
+     
        const  bullets =Array.from( document.querySelectorAll('.bullet'))
       function sliding(){
         slider.style.left = `-${sliderCards[currentSlide].offsetLeft}px`;
@@ -28,7 +36,7 @@ const slider = document.querySelector('.slider'),
         }
         sliding();
       
-      },4500)
+      },9500)
 
       bullets.map(el =>{
         el.addEventListener("click", () => {
@@ -41,6 +49,65 @@ const slider = document.querySelector('.slider'),
               currentSlide = 0;
             }
             sliding();
-          },4500)
+          },9500)
         })
       })
+   
+      for(let i in planetName ) {
+      const planet = Object.values(planets);
+       // planetName[i].textContent = planet[i].name;
+          planetDetails[i].innerHTML =`
+       <tr>
+        <td>name:</td>
+        <td>${planet[i].name}</td>
+       </tr>
+       <tr>
+        <td>rotation:</td> 
+        <td>${planet[i].rotation}</td>
+       </tr>
+        <tr>
+        <td>day:</td>
+        <td>${planet[i].day}</td>
+       </tr>
+        <tr>
+        <td>distance:</td> 
+        <td>${planet[i].distance}</td>
+       </tr> 
+        <tr>
+        <td>moons:</td>
+        <td>${planet[i].moons}</td>
+       </tr>
+        <tr>
+        <td>gravity:</td>
+        <td>${planet[i].gravity}</td>
+       </tr>
+        `
+        planetsDescription[i].textContent = planet[i].description
+      }
+      
+     
+       let start,
+           end
+      slider.addEventListener('touchstart', (e) => start = e.changedTouches[0].pageX )
+      slider.addEventListener('touchend', (e) =>{ end = e.changedTouches[0].pageX 
+        if (start-end > 0){
+          console.log(currentSlide)
+          if(currentSlide = sliderCards.length){
+              currentSlide = 0;}
+          currentSlide++;
+          sliding()
+        }else if(start-end < 0){
+          console.log(currentSlide)
+          if(currentSlide <= 0){
+              currentSlide = sliderCards.length;}
+          currentSlide--;
+          sliding();
+          }
+      })
+        
+      
+       
+       
+       
+     
+     
--- planets.js
+++ planets.js
@@ -1,51 +1,242 @@
-const planets = {
-    mercury:{
-        name:'mercury',
-        rotation: 1407.6,
+export const planets = {
+  mercury: {
+    name: "mercury",
+    rotation: 1407.6,
+    day: 4222.6,
+    distance: 57.9,
+    moons: 0,
+    gravity: 3.7,
+    description: `
+        Mercury is a small planet which orbits closer to the sun than
+        any other planet in our solar system.
         
-
-    },
-    venus:{
-        name:'venus',
-        rotation: -5832.5,
+        • Mercury has no moons.
         
-
-    },
-    earth:{
-        name:'earth',
-        rotation: 23.9,
+        • Mercury’s surface is very hot, it features a barren, crater
+        covered surface which looks similar to Earth’s moon.
         
-
-    },
-    mars:{
-        name:'mars',
-        rotation: 24.6,
+        • Mercury is so close to the Sun, the daytime temperature is scorching
+        reaching over 400 degrees Celsius.
         
-
-    },
-    jupiter:{
-        name:'jupiter',
-        rotation: 9.9,
+        • At night however, without an atmosphere to hold heat in, the
+        temperatures plummet, dropping to -180 degrees Celsius.
         
-
-    },
-    saturn:{
-        name:'saturn',
-        rotation: 10.7,
+        • Mercury has a very low surface gravity.
         
-
-    },
-    uranus:{
-        name:'uranus',
-        rotation: -17.2,
+        • Mercury has no atmosphere which means there is no wind or
+        weather to speak of.
         
+        • Mercury has no water or air on the surface.`,
+  },
+  venus: {
+    name: "venus",
+    rotation: -5832.5,
+    day: 2802,
+    distance: 108.2,
+    moons: 0,
+    gravity: 8.9,
+    description: `
+        Venus is named after the Roman goddess of
+        love and beauty.
+        • Venus is the second planet from the sun.
+        • Is the brightest object in the sky besides our
+        Sun and the Moon.
+        Venus’ symbol
+        • Has no moons.
+        • It is also known as the morning star because at sunrise it appears
+        in the east. It is also known as evening star as it appears at sunset
+        when it is in the west. It cannot be seen in the middle of the night.
+        • Venus and Earth are close together in space and similar in size,
+        which is the reason Venus is called Earth's sister planet.
+        • Venus has more volcanoes than any other planet.
+        • It is the hottest planet in the solar system, even hotter than
+        Mercury, which is closer to the Sun.
+        • The temperature on the surface of Venus is about 460° Celsius.
+        • The atmosphere on Venus is composed of carbon dioxide. The
+        surface is heated by radiation from the sun, but the heat cannot
+        escape through the clouds and layer of carbon dioxide. (This is a
+        “greenhouse effect”).`,
+  },
+  earth: {
+    name: "earth",
+    rotation: 23.9,
+    day: 24,
+    distance: 149.6,
+    moons: 1,
+    gravity: 9.8,
+    description: `
+        • All of the planets, except for Earth, were
+        named after Greek and Roman gods and
+        goddesses. The name Earth is an Old English and
+        German name which simply means soil.
+        •Earth is the third planet from the sun.
+        • Has one moon.
+        • It is the only planet that has liquid water on its surface.
+        • It is the only planet in the solar system that has life.
+        • The Earth is fragile. Its surface is split into plates (tectonic plates)
+        which float on a rocky mantle – the layer between the surface of
+        the earth, its crust, and its hot liquid core. The inside of the Earth
+        is active and earthquakes, volcanoes and mountain building
+        takes place along the boundaries of the tectonic plates.
+        • The Earth is larger than Mercury, Venus and Mars, the planets
+        closest to it.
+        • The Earth differs from all the other planets because it has such a
+        wide diversity of life and intelligent beings. This has only been
+        possible because of the Earth’s atmosphere which has protected
+        the Earth and allowed life to flourish`,
+  },
+  mars: {
+    name: "mars",
+    rotation: 24.6,
+    day: 24.7,
+    distance: 227.9,
+    moons: 2,
+    gravity: 3.7,
+    description: `
+        • Mars was named after the Roman god of war,
+        it is also described as the “Red Planet” because it
+        is covered with rust-like dust.
+        •Mars is the fourth planet from the sun.
+        •It is the second smallest planet in the Solar System.
+        • Has two moons.
+        • Mars looks a lot like our home, though instead of blue oceans
+        and green land, Mars is home to an ever present red tint. This is
+        due to a mineral called iron oxide that is very common on the
+        planet’s surface.
+        • Mars has many massive volcanoes and is home to Olympus
+        Mons, the largest volcano in our solar system, it stands 21km high
+        and is 600km across the base.
+        • Mars has seasons like Earth too. These seasons are much longer
+        than Earth seasons because Mars is so much farther from the sun.
+        • Mars experiences violent dust storms which continually change its
+        surface.
+        • Mars has a very thin atmosphere made mostly of carbon dioxide.
+        It is not thick enough to trap the sun's heat like Venus, so the
+        planet is very cold. Temperatures range from -120 Degrees
+        Celsius on winter nights to 25 Degrees Celsius in the
+        summer.`,
+  },
+  jupiter: {
+    name: "jupiter",
+    rotation: 9.9,
+    day: 9.9,
+    distance: 778.6,
+    moons: 79,
+    gravity: 23.1,
+    description: `
+        •It is called after the ancient Roman sky-god,
+        Jupiter, known to the Greeks as Zeus.
 
-    },
-    neptune:{
-        name:'neptune',
-        rotation: 16.1,
+        •Jupiter is the fifth planet from the sun.
         
-
-    }
-} 
-console.log(Object.entries(planets))
+        •Jupiter is the largest planet in our Solar system. It is
+        so big that more than 1300 Earths could fit inside it.
+        Jupiter’s symbol
+        
+        • Has 63 moons! The first 4 largest moons of Jupiter are called Galilean
+        moons but 46 of them are much smaller ranging.
+        
+        • Jupiter is the stormiest planet in the Solar System. There is a permanent,
+        but ever-changing whirlpool of storms, known as Jupiter’s Great Red
+        Spot which can be seen using a telescope.
+        
+        • The Red Spot was first seen by Robert Hooke in 1664.
+        
+        • Jupiter is the first of the “gas giants”, Jupiter, Saturn, Uranus and
+        Neptune.
+        
+        • Jupiter is made of hydrogen, helium, methane and ammonia. The
+        cloudy sphere has bright belts on it which change their shape.
+        
+        • If you were to descend into Jupiter, the thin, cold atmosphere
+        becomes thicker and hotter, gradually turning into a thick, dark fog. In
+        the blackness about 1000km down the pressure squeezes the
+        atmosphere so hard that it becomes like liquid.`,
+  },
+  saturn: {
+    name: "saturn",
+    rotation: 10.7,
+    day: 10.7,
+    distance: 1433.5,
+    moons: 82,
+    gravity: 9,
+    description: `
+        • Saturn was named after the ancient Roman
+        god of agriculture.
+        • Is the sixth planet from the sun.
+        • Saturn is the second largest planet in our Solar
+        System, after the giant Jupiter.
+        • Has 62 moons!
+        • Saturn is the last planet that can be seen without using a telescope or
+        binoculars and the planet was known in the ancient world before
+        telescopes were invented. The rings, however, can only be seen using
+        a telescope.
+        • The rings were first seen by Galileo in 1610 through a telescope.
+        • The rings are made up of millions of ice crystals, some as big as houses
+        and others as small as specks of dust.
+        • The four largest outer planets, Jupiter, Saturn, Uranus and Neptune, are
+        known as the “gas giants” since it is thought they are entirely made up
+        of dense layers of gas. Saturn is a great ball of hydrogen and helium.
+        • Saturn is very light as it is made up of more hydrogen that helium so it is
+        less dense.
+        • Saturn is not a peaceful planet. Storm winds race around the
+        atmosphere at 800kmp/h.`,
+  },
+  uranus: {
+    name: "uranus",
+    rotation: -17.2,
+    day: 17.2,
+    distance: 2872.5,
+    moons: 27,
+    gravity: 8.7,
+    description: `
+        •Uranus was named after the Greek god of the
+        sky.
+        •Is the seventh planet from the sun.
+        •Uranus was the first planet discovered by
+        telescope.
+        • Has 27 moons.
+        • Uranus was first seen by William Herschel in 1781 during a survey of the
+        sky using a telescope.
+        • Rolls like a barrel rather than spinning like Earth and the other planets in
+        our Solar System.
+        • Uranus is the coldest planet in our Solar System.
+        • Uranus is one of the “gas giants”, the four outer planets which are
+        entirely composed of gas, Jupiter, Saturn and Neptune.
+        • Uranus is the smallest of the four “giants”, but is still several times larger
+        than the Earth.
+        • Uranus’s atmosphere is mostly hydrogen but it also contains large
+        amounts of a gas called methane. Methane absorbs red light and
+        scatters blue light so a blue-green methane haze hides the interior of
+        the planet from view.`,
+  },
+  neptune: {
+    name: "neptune",
+    rotation: 16.1,
+    day: 16.1,
+    distance: 4495.1,
+    moons: 14,
+    gravity: 11,
+    description: `
+        •Neptune is named after the Roman god of the
+sea.
+•Is the eighth planet in the Solar System.
+•Is the farthest planet from the Sun in the Solar
+System.
+• Has 13 moons.
+• Neptune is one of the four “gas giants”. Like Jupiter, Saturn and Uranus,
+it is composed only of gas. Neptune is a great ball of hydrogen and
+helium.
+• In the same year that Neptune was first seen, 1846, its first moon was
+also spotted and named Triton. Triton is a most unusual moon since it
+orbits Neptune in the opposite direction of Neptune’s own rotation on
+its axis. All the other major satellites (moons) in the Solar System follow
+their planets round as they turn.
+• Neptune is a large, water planet with a blue hydrogen-methane
+atmosphere and faint rings.
+• Neptune suffers the most violent weather in our Solar System.
+• Storms have been spotted swirling around its surface and freezing
+winds that blow about ten times faster than hurricanes on Earth making
+it the windiest planet in the Solar System.`,
+  },
+};
